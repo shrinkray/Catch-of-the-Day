7be5d89a7cb070b9b78c283d15b6e7b1c14d8eba
Greg Miller <hello@gregmiller.io>
2021-11-28 10:20:55 -0500

    chore: Move .prettierrc settings to rules in eslint and Add scripts

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -2,11 +2,19 @@ import React from 'react';
 import { getFunName } from '../helpers';
 
 class StorePicker extends React.Component {
+  myInput = React.createRef();
+
+  // eslint-disable-next-line class-methods-use-this
+  goToStore = (event) => {
+    event.preventDefault();
+    console.log(this.myInput.value);
+  };
+
   render() {
     return (
-      <form className="store-selector">
+      <form className="store-selector" onSubmit={this.goToStore}>
         <h2>Please Enter A Store</h2>
-        <input type="text" className="" required placeholder="Store Name" defaultValue={getFunName()} />
+        <input type="text" ref={this.myInput} required placeholder="Store Name" defaultValue={getFunName()} />
         <button type="submit">Visit Store ➡️</button>
       </form>
     );
